d56a6e7b8ad8d1ff448a84f4e6d0181af4ca14c7
Michal Wozniak <2554268w@student.gla.ac.uk>
2022-08-11 00:23:44 +0100

    Added information on the index page

--- playlist-sharer-re/src/components/header/Header.jsx
+++ playlist-sharer-re/src/components/header/Header.jsx
@@ -7,7 +7,8 @@ const Header = () => {
     <header id = "header">   
       <div id = "title_section_of_page">
         <h1>PlaylistSharerRE</h1>  
-        <small>Share , combine and synchronise your playlists!</small> 
+        <small>Share , combine and synchronise your playlists!</small>  
+        <p>*currently only verified test accounts can use the app. Sorry :(</p>
       </div> 
     </header>
   )
